09b64662b8f3a1d9eca679eb462935d0dfa3a443
thatsnotamuffin <bretlrickerson@gmail.com>
2023-04-17 13:05:46 -0500

    Removed install command in favor of upgrade

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ echo "$KUBE_CONFIG" | base64 -d > /tmp/kube_config
 export KUBECONFIG=/tmp/kube_config
 
 sed -i "s/RELEASE_IMAGE/$RELEASE_IMAGE/g" apps/$APPLICATION/$ENVIRONMENT-values.yaml
-helm install $APPLICATION apps/$APPLICATION/ --namespace $KUBE_NAMESPACE -f apps/$APPLICATION/$ENVIRONMENT-values.yaml
+helm upgrade $APPLICATION apps/$APPLICATION/ --namespace $KUBE_NAMESPACE -f apps/$APPLICATION/$ENVIRONMENT-values.yaml
